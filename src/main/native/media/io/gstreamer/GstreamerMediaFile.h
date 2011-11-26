57b603443a4dd5144e2b09568eabfc5aa89eb504
Karl Bennett <where_1ts@yahoo.com>
2011-11-26 23:25:15 +0000

    Added comments to the GstreamerMediaFile.

--- src/main/native/media/io/gstreamer/GstreamerMediaFile.h
+++ src/main/native/media/io/gstreamer/GstreamerMediaFile.h
@@ -9,14 +9,24 @@
 #define GSTREAMERMEDIAFILE_H_
 
 #include <media/io/MediaFile.h>
-#include <glib.h>
-#include <gst/gst.h>
 
 namespace transcode {
 
+/**
+ * MediaFile implemented with the Gstreamer framework.
+ */
 class GstreamerMediaFile: public MediaFile {
 
 	public:
+		/**
+		 * Instantiate a GstreamerMediaFile and extract
+		 * the properties of the media file at the provided path.
+		 *
+		 * @param string - the path to a media file.
+		 *
+		 * @throws MediaFileException if something went wrong
+		 * 		   while extracting properties from the media file.
+		 */
 		GstreamerMediaFile(const std::string&);
 };
 
