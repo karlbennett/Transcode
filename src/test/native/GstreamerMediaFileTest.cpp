4cf3dfe191de97ef161c488e9889c016b685bf1e
Karl Bennett <where_1ts@yahoo.com>
2011-11-27 01:15:58 +0000

    Updated the GstreamerMediaFileTest file to use the new TestUtils file.

--- src/test/native/GstreamerMediaFileTest.cpp
+++ src/test/native/GstreamerMediaFileTest.cpp
@@ -1,26 +1,10 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
+#include <TestUtils.h>
 #include <media/io/gstreamer/GstreamerMediaFile.h>
 #include <media/io/exception/MediaFileException.h>
 #include <string>
 
-// Paths to the test media files.
-const std::string VIDEO_ONE = "../../../target/test-classes/lib-test/test.avi";
-const std::string VIDEO_TWO = "../../../target/test-classes/lib-test/test.mkv";
-const std::string VIDEO_THREE = "../../../target/test-classes/lib-test/test.mp4";
-const std::string TEXT_FILE = "../../../target/test-classes/lib-test/test.txt";
-
-// The media containers that will be tested.
-const std::string AVI_CONTAINER = "video/x-msvideo";
-const std::string MKV_CONTAINER = "video/x-matroska";
-const std::string MP4_CONTAINER = "video/quicktime";
-
-// The audio types that will be tested.
-const std::string MPG_AUDIO = "audio/mpeg";
-
-// The video types that will be tested.
-const std::string DIVX_VIDEO = "video/x-xvid";
-const std::string H264_VIDEO = "video/x-h264";
 
 /**
  * Test to make sure that the GstreamerMediaFile can open all the test media files.
