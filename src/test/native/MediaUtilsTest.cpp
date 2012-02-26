db6364028e21001a8e2089fc3f0a8175fff47e7c
Karl Bennett <where_1ts@yahoo.com>
2012-02-26 17:16:35 +0000

    Created a test file for the MediaUtils file.

---
+++ src/test/native/MediaUtilsTest.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+#include <utils/MediaUtils.hpp>
+
+
+/**
+ * Test example.
+ */
+BOOST_AUTO_TEST_CASE( test_open_media_file )
+{
+
+	transcode::MediaFileDetail medaFile = transcode::findMediaFileDetails("");
+}
