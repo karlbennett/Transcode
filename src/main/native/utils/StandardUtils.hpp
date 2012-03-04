acb213e08af5d9555917d42115d8b8bf66b0942a
Karl Bennett <where_1ts@yahoo.com>
2012-03-04 00:17:04 +0000

    Moved the get templates definition into the StandardUtils header file because template definitions cannot be in a different file to their declerations.

--- src/main/native/utils/StandardUtils.hpp
+++ src/main/native/utils/StandardUtils.hpp
@@ -20,6 +20,21 @@
  * 			exists, otherwise return NULL.
  */
 template<typename K, typename V> V get(const std::map<K, V>& map
-		, const K& key);
+		, const K& key) {
+	// The definition of this template is in the header file because templates must
+	// be declared and defined in the same file.
+
+	// Use the map find function so that we can get the value from a const map.
+	// Code from here:
+	// http://stackoverflow.com/questions/687789/c-const-stdmap-reference-fails-to-compile
+	typename std::map<K, V>::const_iterator it;
+	it = map.find(key);
+	if (it != map.end()) {
+
+		return it->second;
+	}
+
+	return NULL;
+}
 
 #endif /* __STANDARD_UTILS_HPP__ */
