0e87741d7f48f47568dcf4eeff46ca2f4169473d
Karl Bennett <where_1ts@yahoo.com>
2012-03-18 00:12:24 +0000

    Added tests for the util_file API.

---
+++ src/test/native/util_file_test.cpp
@@ -0,0 +1,31 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include <util/util_file.hpp>
+
+#include <util_test.hpp>
+
+/**
+ * Test that a normal file can be opened.
+ */
+BOOST_AUTO_TEST_CASE( test_util_file )
+{
+
+    transcode::utils::File file(TEXT_FILE);
+
+	BOOST_CHECK_EQUAL( file.getPath(), TEXT_FILE );
+	BOOST_CHECK_EQUAL( file.getName(), TEXT_NAME );
+	BOOST_CHECK_EQUAL( file.getSize(), 25 );
+}
+
+/**
+ * Test that trying to open an invalid file throws
+ * an exception.
+ */
+BOOST_AUTO_TEST_CASE( test_util_file_on_invalid_file )
+{
+
+    BOOST_REQUIRE_THROW(
+            transcode::utils::File file(INVALID_FILE),
+            transcode::utils::FileException );
+}
