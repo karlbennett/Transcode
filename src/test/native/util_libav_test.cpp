3f215af06c3b58c3e1f80a7e43ccde6aae43cedd
Karl Bennett <where_1ts@yahoo.com>
2012-05-13 12:02:30 +0100

    Created a test file for the new libav functions.

---
+++ src/test/native/util_libav_test.cpp
@@ -0,0 +1,19 @@
+/*
+ * util_libav_test.cpp
+ *
+ *  Created on: 9 May 2012
+ *      Author: karl
+ */
+
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include <util/util_libav.hpp>
+
+/**
+ * Test example.
+ */
+BOOST_AUTO_TEST_CASE( test_example )
+{
+
+}
