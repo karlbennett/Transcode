f1f430558d821928a5762211b84fcfe6997da1f8
Karl Bennett <where_1ts@yahoo.com>
2012-07-26 22:50:52 +0100

    Moved the static functions in the libav.cpp to the top of the file and grouped them into meaningfull namespaces.

--- src/main/native/libav/libav.cpp
+++ src/main/native/libav/libav.cpp
@@ -38,6 +38,239 @@ namespace libav {
  */
 const int BUFFER_SIZE = AVCODEC_MAX_AUDIO_FRAME_SIZE + FF_INPUT_BUFFER_PADDING_SIZE;
 
+namespace wrappers {
+
+/**
+ * Template that wraps the supplied callback with default error checking and makes an
+ * internal copy of the supplied packet that is then passed into the callback.
+ *
+ * The callback for this function should then decode the copied packet into it's
+ * desired container.
+ *
+ * @param codecContext - the codec context that will be used to decode the callback.
+ * @param packet - the packet that is to be decoded.
+ * @param decodeCallback - the callback that will carry out the actual decode of the packet.
+ * @param T - the return type of the supplied callback.
+ * @return the result of the callback.
+ */
+template<typename T> T decodePacketTemplate(AVCodecContext *codecContext, const AVPacket *packet,
+        std::tr1::function<T(AVCodecContext *codecContext, AVPacket *packet)> decodeCallback) {
+
+    if (NULL == codecContext) {
+
+        throw IllegalArgumentException("The codec context for decoding cannot be null.");
+    }
+
+    if (NULL == packet) {
+
+        throw IllegalArgumentException("The packet for decoding cannot be null.");
+    }
+
+    // A copy of the supplied packet that will be used during the decoding so that we
+    // don't mutate the supplied packet. If we mutated the supplied packet it would no
+    // longer be able to be correctly deleted.
+    AVPacket packetCopy;
+    // Create a new large buffer array to hold the audio packet.
+    uint8_t buffer[BUFFER_SIZE];
+    // Set all the values within to 0 so that there is no question about the data end.
+    memset(buffer, 0, BUFFER_SIZE);
+    // Copy the packet data into the new buffer.
+    memcpy(buffer, packet->data, packet->size);
+    // Set the copy packets data and size.
+    packetCopy.data = buffer;
+    packetCopy.size = packet->size;
+
+    return decodeCallback(codecContext, &packetCopy);
+}
+
+static AVPacket* encodeFrameWrapper(AVCodecContext *codecContext, const AVFrame *frame,
+        std::tr1::function<int(AVCodecContext *codecContext, AVPacket *packet,
+                const AVFrame *frame, int *packetEncoded)> encodeCallback) {
+
+    if (NULL == codecContext) {
+
+        throw IllegalArgumentException("The codec context for encoding cannot be null.");
+    }
+
+    if (NULL == frame) {
+
+        throw IllegalArgumentException("The frame for encoding cannot be null.");
+    }
+
+    AVPacket *packet = new AVPacket();
+
+    packet->size = 0;
+    packet->data = NULL;
+
+    av_init_packet(packet);
+
+    int packetEncoded = 0;
+
+    int result = encodeCallback(codecContext, packet, frame, &packetEncoded);
+
+    if (AVERROR_INVALIDDATA == result) {
+
+        throw InvalidPacketDataException(errorMessage(result));
+    }
+
+    if (0 > result) {
+
+        throw PacketDecodeException(errorMessage(result));
+    }
+
+    if (0 != packetEncoded) return packet;
+
+    return NULL;
+}
+
+}
+
+namespace callbacks {
+
+/**
+ * A callback function that decodes an audio packet. This should be supplied
+ * to the <code>decodePacketTemplate</code> template.
+ *
+ * @param codecContext - the codec context that will be used to decode the callback.
+ * @param packet - the packet that is to be decoded.
+ * @return the decoded packet as a vector of AVFrames.
+ */
+static vector<AVFrame*> decodeAudioPacketCallback(AVCodecContext *codecContext,
+        AVPacket *packet) {
+
+    if (AVMEDIA_TYPE_AUDIO != findCodecType(codecContext)) {
+
+        throw IllegalArgumentException(
+                "The supplied codec context for decoding audio must have media type AVMEDIA_TYPE_AUDIO.");
+    }
+
+    // This vector will contain all the audio frames decoded from the supplied packet.
+    vector<AVFrame*> frames;
+
+    // The frame pointer that will hold each new frame before it is placed in the vector.
+    AVFrame *decodedFrame = NULL;
+
+    // This will be set to 1 if a frame has successfully been decoded with the
+    // avcodec_decode_audio4() function.
+    int frameDecoded = 0;
+
+    int bytesDecoded = 0; // The number of bytes that were decoded in each iteration.
+
+    while (0 < packet->size) {
+        // Create a new frame to contain the decoded data if one is required.
+        if (NULL == decodedFrame) decodedFrame = avcodec_alloc_frame();
+
+        // Decode the packet and store it in the new frame.
+        // Also record how many bytes were decoded because it might not have been all
+        // of them.
+        bytesDecoded = avcodec_decode_audio4(codecContext, decodedFrame,
+                &frameDecoded,
+                packet);
+
+        // If there is an invalid data error throw a more specific exception.
+        if (AVERROR_INVALIDDATA == bytesDecoded) {
+
+            throw InvalidPacketDataException(errorMessage(bytesDecoded));
+        }
+
+        if (0 > bytesDecoded) {
+
+            throw PacketDecodeException(errorMessage(bytesDecoded));
+        }
+
+        // If a frame was successfully decoded add it the vector to be returned and
+        // set the pointer to null to indicate we need a new frame allocated.
+        if (0 != frameDecoded) {
+
+            frames.push_back(decodedFrame);
+
+            decodedFrame = NULL;
+
+        } else {
+            // If we haven't successfully decoded a frame reset the decode frame
+            // values to make sure it's ready for another decode attempt.
+            avcodec_get_frame_defaults(decodedFrame);
+        }
+
+        // Push the data pointer down the byte array passed the last byte that we
+        // decoded.
+        packet->data += bytesDecoded;
+        // Reduce the relative size of the data to the amount that is yet to be
+        // decoded.
+        packet->size -= bytesDecoded;
+    }
+
+    return frames;
+}
+
+static int encodeAudioFrameCallback(AVCodecContext *codecContext, AVPacket *packet,
+                const AVFrame *frame, int *packetEncoded) {
+
+    if (AVMEDIA_TYPE_AUDIO != findCodecType(codecContext)) {
+
+        throw IllegalArgumentException(
+                "The supplied codec context for encoding audio must have media type AVMEDIA_TYPE_AUDIO.");
+    }
+
+    return avcodec_encode_audio2(codecContext, packet, frame, packetEncoded);
+}
+
+/**
+ * A callback function that decodes a video packet. This should be supplied
+ * to the <code>decodePacketTemplate</code> template.
+ *
+ * @param codecContext - the codec context that will be used to decode the callback.
+ * @param packet - the packet that is to be decoded.
+ * @return the decoded packet as an AVFrame.
+ */
+static AVFrame* decodeVideoPacketCallBack(AVCodecContext *codecContext,
+        AVPacket *packet) {
+
+    if (AVMEDIA_TYPE_VIDEO != findCodecType(codecContext)) {
+
+        throw IllegalArgumentException(
+                "The supplied codec context for decodeVideoPacket(AVCodecContext*,AVPacket*) must have media type AVMEDIA_TYPE_VIDEO.");
+    }
+
+    AVFrame *decodedFrame = avcodec_alloc_frame();
+
+    int bytesDecoded = 0;
+
+    int frameDecoded = 0;
+
+    bytesDecoded = avcodec_decode_video2(codecContext, decodedFrame,
+            &frameDecoded,
+            packet);
+
+    if (AVERROR_INVALIDDATA == bytesDecoded) {
+
+        throw InvalidPacketDataException(errorMessage(bytesDecoded));
+    }
+
+    if (0 > bytesDecoded) {
+
+        throw PacketDecodeException(errorMessage(bytesDecoded));
+    }
+
+    if (0 != frameDecoded) return decodedFrame;
+
+    return NULL;
+}
+
+static int encodeVideoFrameCallback(AVCodecContext *codecContext, AVPacket *packet,
+                const AVFrame *frame, int *packetEncoded) {
+
+    if (AVMEDIA_TYPE_VIDEO != findCodecType(codecContext)) {
+
+        throw IllegalArgumentException(
+                "The supplied codec context for encoding video must have media type AVMEDIA_TYPE_VIDEO.");
+    }
+
+    return avcodec_encode_video2(codecContext, packet, frame, packetEncoded);
+}
+
+}
+
 class LibavSingleton
 {
 
@@ -321,256 +554,33 @@ void LibavSingleton::closeCodecContext(AVCodecContext **codecContext) const {
     throw CodecException(errorMessage(codecCloseResult));
 }
 
-/**
- * Template that wraps the supplied callback with default error checking and makes an
- * internal copy of the supplied packet that is then passed into the callback.
- *
- * The callback for this function should then decode the copied packet into it's
- * desired container.
- *
- * @param codecContext - the codec context that will be used to decode the callback.
- * @param packet - the packet that is to be decoded.
- * @param decodeCallback - the callback that will carry out the actual decode of the packet.
- * @param T - the return type of the supplied callback.
- * @return the result of the callback.
- */
-template<typename T> T decodePacketTemplate(AVCodecContext *codecContext, const AVPacket *packet,
-        std::tr1::function<T(AVCodecContext *codecContext, AVPacket *packet)> decodeCallback) {
-
-    if (NULL == codecContext) {
-
-        throw IllegalArgumentException("The codec context for decoding cannot be null.");
-    }
-
-    if (NULL == packet) {
-
-        throw IllegalArgumentException("The packet for decoding cannot be null.");
-    }
-
-    // A copy of the supplied packet that will be used during the decoding so that we
-    // don't mutate the supplied packet. If we mutated the supplied packet it would no
-    // longer be able to be correctly deleted.
-    AVPacket packetCopy;
-    // Create a new large buffer array to hold the audio packet.
-    uint8_t buffer[BUFFER_SIZE];
-    // Set all the values within to 0 so that there is no question about the data end.
-    memset(buffer, 0, BUFFER_SIZE);
-    // Copy the packet data into the new buffer.
-    memcpy(buffer, packet->data, packet->size);
-    // Set the copy packets data and size.
-    packetCopy.data = buffer;
-    packetCopy.size = packet->size;
-
-    return decodeCallback(codecContext, &packetCopy);
-}
-
-static AVPacket* encodeFrameWrapper(AVCodecContext *codecContext, const AVFrame *frame,
-        std::tr1::function<int(AVCodecContext *codecContext, AVPacket *packet,
-                const AVFrame *frame, int *packetEncoded)> encodeCallback) {
-
-    if (NULL == codecContext) {
-
-        throw IllegalArgumentException("The codec context for encoding cannot be null.");
-    }
-
-    if (NULL == frame) {
-
-        throw IllegalArgumentException("The frame for encoding cannot be null.");
-    }
-
-    AVPacket *packet = new AVPacket();
-
-    packet->size = 0;
-    packet->data = NULL;
-
-    av_init_packet(packet);
-
-    int packetEncoded = 0;
-
-    int result = encodeCallback(codecContext, packet, frame, &packetEncoded);
-
-    if (AVERROR_INVALIDDATA == result) {
-
-        throw InvalidPacketDataException(errorMessage(result));
-    }
-
-    if (0 > result) {
-
-        throw PacketDecodeException(errorMessage(result));
-    }
-
-    if (0 != packetEncoded) return packet;
-
-    return NULL;
-}
-
-/**
- * A callback function that decodes an audio packet. This should be supplied
- * to the <code>decodePacketTemplate</code> template.
- *
- * @param codecContext - the codec context that will be used to decode the callback.
- * @param packet - the packet that is to be decoded.
- * @return the decoded packet as a vector of AVFrames.
- */
-static vector<AVFrame*> decodeAudioPacketCallback(AVCodecContext *codecContext,
-        AVPacket *packet) {
-
-    if (AVMEDIA_TYPE_AUDIO != findCodecType(codecContext)) {
-
-        throw IllegalArgumentException(
-                "The supplied codec context for decoding audio must have media type AVMEDIA_TYPE_AUDIO.");
-    }
-
-    // This vector will contain all the audio frames decoded from the supplied packet.
-    vector<AVFrame*> frames;
-
-    // The frame pointer that will hold each new frame before it is placed in the vector.
-    AVFrame *decodedFrame = NULL;
-
-    // This will be set to 1 if a frame has successfully been decoded with the
-    // avcodec_decode_audio4() function.
-    int frameDecoded = 0;
-
-    int bytesDecoded = 0; // The number of bytes that were decoded in each iteration.
-
-    while (0 < packet->size) {
-        // Create a new frame to contain the decoded data if one is required.
-        if (NULL == decodedFrame) decodedFrame = avcodec_alloc_frame();
-
-        // Decode the packet and store it in the new frame.
-        // Also record how many bytes were decoded because it might not have been all
-        // of them.
-        bytesDecoded = avcodec_decode_audio4(codecContext, decodedFrame,
-                &frameDecoded,
-                packet);
-
-        // If there is an invalid data error throw a more specific exception.
-        if (AVERROR_INVALIDDATA == bytesDecoded) {
-
-            throw InvalidPacketDataException(errorMessage(bytesDecoded));
-        }
-
-        if (0 > bytesDecoded) {
-
-            throw PacketDecodeException(errorMessage(bytesDecoded));
-        }
-
-        // If a frame was successfully decoded add it the vector to be returned and
-        // set the pointer to null to indicate we need a new frame allocated.
-        if (0 != frameDecoded) {
-
-            frames.push_back(decodedFrame);
-
-            decodedFrame = NULL;
-
-        } else {
-            // If we haven't successfully decoded a frame reset the decode frame
-            // values to make sure it's ready for another decode attempt.
-            avcodec_get_frame_defaults(decodedFrame);
-        }
-
-        // Push the data pointer down the byte array passed the last byte that we
-        // decoded.
-        packet->data += bytesDecoded;
-        // Reduce the relative size of the data to the amount that is yet to be
-        // decoded.
-        packet->size -= bytesDecoded;
-    }
-
-    return frames;
-}
-
-static int encodeAudioFrameCallback(AVCodecContext *codecContext, AVPacket *packet,
-                const AVFrame *frame, int *packetEncoded) {
-
-    if (AVMEDIA_TYPE_AUDIO != findCodecType(codecContext)) {
-
-        throw IllegalArgumentException(
-                "The supplied codec context for encoding audio must have media type AVMEDIA_TYPE_AUDIO.");
-    }
-
-    return avcodec_encode_audio2(codecContext, packet, frame, packetEncoded);
-}
-
-/**
- * A callback function that decodes a video packet. This should be supplied
- * to the <code>decodePacketTemplate</code> template.
- *
- * @param codecContext - the codec context that will be used to decode the callback.
- * @param packet - the packet that is to be decoded.
- * @return the decoded packet as an AVFrame.
- */
-static AVFrame* decodeVideoPacketCallBack(AVCodecContext *codecContext,
-        AVPacket *packet) {
-
-    if (AVMEDIA_TYPE_VIDEO != findCodecType(codecContext)) {
-
-        throw IllegalArgumentException(
-                "The supplied codec context for decodeVideoPacket(AVCodecContext*,AVPacket*) must have media type AVMEDIA_TYPE_VIDEO.");
-    }
-
-    AVFrame *decodedFrame = avcodec_alloc_frame();
-
-    int bytesDecoded = 0;
-
-    int frameDecoded = 0;
-
-    bytesDecoded = avcodec_decode_video2(codecContext, decodedFrame,
-            &frameDecoded,
-            packet);
-
-    if (AVERROR_INVALIDDATA == bytesDecoded) {
-
-        throw InvalidPacketDataException(errorMessage(bytesDecoded));
-    }
-
-    if (0 > bytesDecoded) {
-
-        throw PacketDecodeException(errorMessage(bytesDecoded));
-    }
-
-    if (0 != frameDecoded) return decodedFrame;
-
-    return NULL;
-}
-
-static int encodeVideoFrameCallback(AVCodecContext *codecContext, AVPacket *packet,
-                const AVFrame *frame, int *packetEncoded) {
-
-    if (AVMEDIA_TYPE_VIDEO != findCodecType(codecContext)) {
-
-        throw IllegalArgumentException(
-                "The supplied codec context for encoding video must have media type AVMEDIA_TYPE_VIDEO.");
-    }
-
-    return avcodec_encode_video2(codecContext, packet, frame, packetEncoded);
-}
-
 vector<AVFrame*> LibavSingleton::decodeAudioPacket(
         AVCodecContext *codecContext,
         const AVPacket *packet) const {
 
-    return decodePacketTemplate<vector<AVFrame*> >(codecContext, packet,
-            decodeAudioPacketCallback);
+    return wrappers::decodePacketTemplate<vector<AVFrame*> >(codecContext, packet,
+            callbacks::decodeAudioPacketCallback);
 }
 
 AVPacket* LibavSingleton::encodeAudioFrame(AVCodecContext *codecContext,
         const AVFrame *frame) const {
 
-    return encodeFrameWrapper(codecContext, frame, encodeAudioFrameCallback);
+    return wrappers::encodeFrameWrapper(codecContext, frame,
+            callbacks::encodeAudioFrameCallback);
 }
 
 AVFrame* LibavSingleton::decodeVideoPacket(AVCodecContext *codecContext,
         const AVPacket *packet) const {
 
-    return decodePacketTemplate<AVFrame*>(codecContext, packet,
-            decodeVideoPacketCallBack);
+    return wrappers::decodePacketTemplate<AVFrame*>(codecContext, packet,
+            callbacks::decodeVideoPacketCallBack);
 }
 
 AVPacket* LibavSingleton::encodeVideoFrame(AVCodecContext *codecContext,
         const AVFrame *frame) const {
 
-    return encodeFrameWrapper(codecContext, frame, encodeVideoFrameCallback);
+    return wrappers::encodeFrameWrapper(codecContext, frame,
+            callbacks::encodeVideoFrameCallback);
 }
 
 
